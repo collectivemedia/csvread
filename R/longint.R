adbc041a85c16c60f91e2bfe4016f38530af360c
Sergei Izrailev <sizrailev@jabiruventures.com>
2018-12-05 00:39:01 -0500

    updated version to 1.3 and copyrights to 2018

--- R/longint.R
+++ R/longint.R
@@ -4,9 +4,10 @@
 #
 # int64 class - not all numeric functionality is implemented.
 #
-# Sergei Izrailev, 2011-2014
+# Sergei Izrailev, 2011-2018
 #-------------------------------------------------------------------------------
 # Copyright 2011-2014 Collective, Inc.
+# Copyright 2015, 2018 Jabiru Ventures LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
